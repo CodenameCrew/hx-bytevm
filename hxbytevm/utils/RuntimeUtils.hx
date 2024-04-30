572abe534a0cd95fd35b732eaf07a4ff02fe0052
NeeEoo <NeeEoo@users.noreply.github.com>
2024-04-30 16:25:20 +0200

    Small formatting fixes

--- hxbytevm/utils/RuntimeUtils.hx
+++ hxbytevm/utils/RuntimeUtils.hx
@@ -1,7 +1,7 @@
 package hxbytevm.utils;
 
 class RuntimeUtils {
-    public static inline function iterator(v:Dynamic):Iterator<Dynamic> {
+	public static inline function iterator(v:Dynamic):Iterator<Dynamic> {
 		#if ((flash && !flash9) || (php && !php7 && haxe_ver < '4.0.0'))
 		if (v.iterator != null)
 			v = v.iterator();
@@ -17,18 +17,16 @@ class RuntimeUtils {
 		return v;
 	}
 
-    public static inline function keyValueIterator(v:Dynamic):Iterator<Dynamic> {
+	public static inline function keyValueIterator(v:Dynamic):Iterator<Dynamic> {
 		#if ((flash && !flash9) || (php && !php7 && haxe_ver < '4.0.0'))
 		if (v.keyValueIterator != null)
 			v = v.keyValueIterator();
-
-		//if (v.iterator != null)
-		//	v = v.iterator();
 		#else
-		try {
-			if (v.hasNext == null || v.next == null)
+		if (v.hasNext == null || v.next == null) {
+			try {
 				v = v.keyValueIterator();
-		} catch (e:Dynamic) {}
+			} catch (e:Dynamic) {}
+		}
 		#end
 		if (v.hasNext == null || v.next == null)
 			throw "Invalid Iterator";
