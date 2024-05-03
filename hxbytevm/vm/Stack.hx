1c7de8720a0182b0509155627458addfd7772099
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2024-05-03 16:20:48 -0700

    cpp array and unsafe get/set

--- hxbytevm/vm/Stack.hx
+++ hxbytevm/vm/Stack.hx
@@ -1,30 +1,39 @@
 package hxbytevm.vm;
 
+#if cpp
+import cpp.NativeArray;
+#end
+
 class Stack {
 	public function new() {}
 
 	public static final DEFAULT_STACK_SIZE:Int = 8;
 	public static final MAX_STACK_GROW:Int = 32;
 
-	public var stack:Array<Dynamic> = [for (i in 0...DEFAULT_STACK_SIZE) null];
+	public var stack:Array<Dynamic> = #if cpp NativeArray.create(DEFAULT_STACK_SIZE) #else [for (i in 0...DEFAULT_STACK_SIZE) null] #end;
 	public var stackTop:Int = 0;
 
 	public inline function push(v:Dynamic) {
 		if (stackTop >= (stack.length)) grow();
-		stack[stackTop] = v;
-		return stack[(stackTop++) - 1];
+		untyped stack.__unsafe_set(stackTop, v);
+		return untyped stack.__unsafe_get((stackTop++) - 1);
 	}
 
 	public inline function pop():Dynamic {
 		stackTop--;
+		#if cpp
+		var ret = untyped stack.__unsafe_get(stackTop);
+		untyped stack.__unsafe_set(stackTop, null);
+		#else
 		var ret = stack[stackTop];
 		stack[stackTop] = null;
+		#end
 		return ret;
 	}
 
 	public inline function grow()
-		stack.resize(stack.length + (stack.length > MAX_STACK_GROW ? MAX_STACK_GROW : stack.length));
+		#if cpp untyped stack.__SetSizeExact #else stack.resize #end (stack.length + (stack.length > MAX_STACK_GROW ? MAX_STACK_GROW : stack.length));
 
 	public inline function top():Dynamic
-		return stack[stackTop-1];
+		return #if cpp untyped stack.__unsafe_get(stackTop-1) #else stack[stackTop-1] #end;
 }
